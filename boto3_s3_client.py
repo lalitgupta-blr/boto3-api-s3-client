4c06f225b22e0ae03e312cb8641821db842ef78d
Lalit Gupta <lalit.gupta@olodeck.com>
2020-03-15 20:08:01 +0530

    Added Boto3 client and Requirements file

--- boto3_s3_client.py
+++ boto3_s3_client.py
@@ -8,12 +8,23 @@ S3_BUCKET = '***'
 s3_client = boto3.client('s3',aws_access_key=S3_ACCESS_ID, aws_secret_access_key=S3_SECRET_ACCESS_KEY, region_name='eu-west-1')
 s3_resource = boto3.resource('s3',aws_access_key=S3_ACCESS_ID, aws_secret_access_key=S3_SECRET_ACCESS_KEY, region_name='eu-west-1')
 
+
+
+response = s3_client.list_buckets()
+
+# Output the bucket names
+print('Existing buckets:')
+for bucket in response['Buckets']:
+    print(f'  {bucket["Name"]}')
+
+
 bucket = s3_resource.Bucket(S3_BUCKET)
 
 
 for bucket_object in bucket.objects.all()
 	if bucket_object.key.lower().endswith('.txt'):
-			process_file(bucket_object)
+		print("Contents of Key: " + bucket_object.key)
+		process_file(bucket_object.key)
 
 
 def process_file(key):
